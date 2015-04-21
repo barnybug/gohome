613cface40ae6d6820c329fe449e187b1bb0753c
Barnaby Gray <barnaby@pickle.me.uk>
2015-04-21 22:50:20 +0100

    Cross build updates

--- crossbuild/build.sh
+++ crossbuild/build.sh
@@ -10,7 +10,5 @@ GOOS=linux GOARCH=386 goop go build -v -o release/gohome-linux-386 $EXE
 GOOS=linux GOARCH=arm GOARM=5 goop go build -v -o release/gohome-linux-arm5 $EXE
 
 # compress resulting executables
-# workaround for bug (see: https://github.com/pwaller/goupx)
-/go/bin/goupx -u=false release/gohome-linux-amd64
 upx release/*
 chmod -R a+rw release
