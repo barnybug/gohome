764e4d4cdae01c461dcb9bfdea118ff8c9efd9cc
Barnaby Gray <barnaby@pickle.me.uk>
2017-02-17 21:21:39 +0000

    Fix tests on travis

--- services/hwmon/main_test.go
+++ services/hwmon/main_test.go
@@ -13,13 +13,24 @@ func ExampleInterfaces() {
 	// Output:
 }
 
+func hasThermalZones() bool {
+	matches, _ := filepath.Glob("/sys/devices/virtual/thermal/thermal_zone?/temp")
+	return len(matches) > 0
+}
+
 func TestFindThermalDevices(t *testing.T) {
+	if !hasThermalZones() {
+		return
+	}
 	zones, err := findThermalDevices()
 	assert.NoError(t, err, "No error returned")
 	assert.NotEmpty(t, zones, "Zones should contain entries")
 }
 
 func TestReadTemp(t *testing.T) {
+	if !hasThermalZones() {
+		return
+	}
 	matches, err := filepath.Glob("/sys/devices/virtual/thermal/thermal_zone?/temp")
 	assert.NoError(t, err, "No error returned")
 
