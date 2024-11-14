a65dbc928f0ea24fe3b5ad32a239ba6b553400e3
Barnaby Gray <barnaby@pickle.me.uk>
2024-11-14 06:58:41 +0000

    Flake home manager module

--- flake.nix
+++ flake.nix
@@ -11,6 +11,7 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in {
+        # System specific outputs becomes: packages.<system>.default
         packages.default = pkgs.buildGoModule {
           pname = "gohome";
           version = "0.0.1";
@@ -18,5 +19,66 @@
           vendorHash = "sha256-JxToHZUROCvffMN9hZy/W7m2G9w/GaxHU9fY4sQqCuQ=";
         };
       }
-    );
+    ) // {
+      # Flake-wide outputs
+      homeManagerModules = {
+        default = { config, lib, pkgs, system, ... }:
+        with lib;
+        let
+          cfg = config.programs.gohome;
+          pkg = self.packages.${pkgs.hostPlatform.system}.default;
+        in
+        {
+          options = {
+            programs.gohome = {
+              enable = mkEnableOption "Gohome";
+              mqtt = mkOption {
+                type = types.str;
+                default = "tcp://mqtt:1883";
+                description = "mqtt url";
+              };
+              services = mkOption {
+                type = types.listOf types.str;
+                default = [];
+                description = "List of gohome services to enable";
+              };
+            };
+          };
+          config = mkMerge [
+            (mkIf cfg.enable {
+              home.packages = [ pkg ];
+            })
+            (mkIf (length cfg.services != 0) {
+              systemd.user.enable = true;
+              # home manager services doesn't appear to support templated services, so
+              # produce a service unit per instance.
+              systemd.user.services = let
+                makeService = (n: {
+                  name = "gohome@${n}";
+                  value = {
+                    Install.WantedBy = [ "default.target" ];
+                    Unit = {
+                      Description = "gohome service ${n}";
+                      StartLimitIntervalSec = "0";
+                      After = [ "network-online.service" ];
+                      Wants = [ "network-online.service" ];
+                    };
+                    Service = {
+                      Environment = [ "GOHOME_MQTT=${cfg.mqtt}" "GOHOME_API=http://localhost:8723/" ];
+                      ExecStart = "${pkg}/bin/gohome run ${n}";
+                      Restart = "always";
+                      RestartSec = "5s";
+                      WatchdogSec = "120s";
+                      Type = "notify";
+                      NotifyAccess = "main";
+                    };
+                  };
+                });
+              in
+                listToAttrs (map makeService cfg.services);
+            })
+          ];
+        }; 
+      };
+    };
 }
