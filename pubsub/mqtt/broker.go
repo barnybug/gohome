d694f2b48de76b2bf888dd54eee04b0095d1c423
Barnaby Gray <barnaby@pickle.me.uk>
2024-11-27 18:44:44 +0000

    Retry initial mqtt connection

--- pubsub/mqtt/broker.go
+++ pubsub/mqtt/broker.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/barnybug/gohome/pubsub"
 	MQTT "github.com/eclipse/paho.mqtt.golang"
@@ -34,6 +35,8 @@ func createClientOpts(broker, name string, persist bool) *MQTT.ClientOptions {
 	return opts
 }
 
+const RETRIES = 20
+
 func NewBroker(broker, name string) *Broker {
 	persist := false
 	opts := createClientOpts(broker, name, persist)
@@ -43,8 +46,19 @@ func NewBroker(broker, name string) *Broker {
 	opts.SetOnConnectHandler(ret.subscriber.connectHandler)
 
 	client := MQTT.NewClient(opts)
-	if token := client.Connect(); token.Wait() && token.Error() != nil {
-		log.Fatalln("Couldn't Start mqtt:", token.Error())
+	for retry := 0; retry < RETRIES; retry++ {
+		if token := client.Connect(); token.Wait() && token.Error() != nil {
+			if retry == 5 {
+				log.Printf("Delay connecting to mqtt: %s, continuing.", token.Error())
+			}
+			if retry < RETRIES-1 {
+				time.Sleep(time.Second)
+				continue
+			}
+			log.Fatalln("Couldn't Start mqtt:", token.Error())
+		} else {
+			break
+		}
 	}
 	Client = client
 	ret.client = client
